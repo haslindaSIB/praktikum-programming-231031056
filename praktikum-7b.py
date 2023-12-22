8824a14be15c790b01252fafbe4849943ace6e0a
haslindaSIB <153716238+haslindaSIB@users.noreply.github.com>
2023-12-22 20:47:47 +0800

    Add files via upload
    praktikum-7b

---
+++ praktikum-7b.py
@@ -0,0 +1,22 @@
+import os
+os.system('clear')
+
+a = True
+
+while a:
+    jawab = input('Apakah ingin lanjutkan? (y/n)')
+    if jawab == 'y':
+
+        os.system('clear')
+        print('Selamat Datang Lagi')
+        a = True
+
+    elif jawab == 'n':
+
+        print('Sampai Ketemu Lagi :D')
+        a = False
+    else:
+
+        os.system('clear')
+
+    _
