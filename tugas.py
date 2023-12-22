68dd146e5ebe3babd12d0bb248dacba2165038a4
haslindaSIB <153716238+haslindaSIB@users.noreply.github.com>
2023-12-22 20:55:25 +0800

    Add files via upload
    tugas

---
+++ tugas.py
@@ -0,0 +1,14 @@
+bio_data = {"nama" : "Haslinda",
+           "prodi" : "Sistem Informasi",
+           "kelas" :"SI23-B",
+           "nim"  : "231031056",
+           "asal"  : "Sadar",
+           "tanggal" : "31-12-2005",
+           }
+
+print("Nama Lengkap     :",bio_data["nama"])
+print("Prodi            :",bio_data["prodi"])
+print("Kelas            :",bio_data["kelas"])
+print("NIM              :",bio_data["nim"])
+print("Tmpt tgl lahir   :",bio_data["asal"],bio_data["tanggal"])
+print("\nTipe data dari var bio_data  : ",type(bio_data))
